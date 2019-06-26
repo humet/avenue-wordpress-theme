68db1b1835aa3cdf843dadd46c3883cdacec8338
Ken Tsang <ken@mediachain.co>
2019-06-26 13:38:16 +0100

    more updates

--- resources/views/partials/content-contact-button.blade.php
+++ resources/views/partials/content-contact-button.blade.php
@@ -1,2 +1,13 @@
-@php the_content() @endphp
-{!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+<div class="pages__contact-buttons">
+        <div class="container row">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        </div>
+        <div class="container row">
+            <a href="#" class="btn apply">
+                apply
+            </a>
+            <a href="#" class="btn register-as-a-business">
+                    register as a business
+            </a>
+        </div>
+    </div>
--- resources/views/partials/content-homepage-buttons.blade.php
+++ resources/views/partials/content-homepage-buttons.blade.php
@@ -1,5 +1,18 @@
 <div class="pages__homepage-buttons">
+    <div class="container row">
+        <a href="#" class="btn find-your-avenue">
+            Find your Avenue
+        </a>
+        <a href="#" class="btn avenue-for-brands">
+                Avenue For Brands
+        </a>
+    </div>
     <div class="container">
-        Hello
+        {{-- <div class="animated-words">
+            <div class="text">NO DEBT</div>
+            <div class="text">NO CLASSROOMS</div>
+            <div class="text">RELEVANT LEARNING</div>
+            <div class="text">WELFARE & LIFE SKILLS</div>
+        </div>     --}}
     </div>
 </div>
