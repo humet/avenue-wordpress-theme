ff390b9f7520804b298b0f30220d0ec1fe2b4ef2
Ken Tsang <ken@mediachain.co>
2019-06-27 13:30:08 +0100

    team pages setup

---
+++ resources/views/partials/content-team-profile.blade.php
@@ -0,0 +1,22 @@
+<div class="pages__team-profile">
+    <div class="row">
+        <div class="pages__team-photo">photo</div>
+        <div class="pages__team-content">content</div>
+    </div>
+    <div class="row reverse">
+        <div class="pages__team-photo">photo</div>
+        <div class="pages__team-content">content</div>
+    </div>
+    <div class="row">
+        <div class="pages__team-photo">photo</div>
+        <div class="pages__team-content">content</div>
+    </div>
+    <div class="row reverse">
+        <div class="pages__team-photo">photo</div>
+        <div class="pages__team-content">content</div>
+    </div>
+    <div class="row">
+        <div class="pages__team-photo">photo</div>
+        <div class="pages__team-content">content</div>
+    </div>
+</div>
