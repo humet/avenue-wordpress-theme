eb872e75132d81e158e8d19d78c8e4b1776b2a78
Ken Tsang <ken@mediachain.co>
2019-06-27 12:17:26 +0100

    more updates

--- resources/views/partials/content-brands-button.blade.php
+++ resources/views/partials/content-brands-button.blade.php
@@ -2,4 +2,9 @@
     <div class="text">
 We don’t give you apprentices. We give you passionate people who are matched to your culture, learning relevant skills to your goals.
     </div>
+    <div class="container row">
+    <a href="#" class="btn download-pdf alt-arow">
+        download PDF
+    </a>
+    </div>
 </div>
