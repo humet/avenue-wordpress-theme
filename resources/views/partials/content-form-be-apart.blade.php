dd76b512b04928734518493e41adae642b5cacd0
Ken Tsang <ken@mediachain.co>
2019-07-02 16:31:09 +0100

    updates

--- resources/views/partials/content-form-be-apart.blade.php
+++ resources/views/partials/content-form-be-apart.blade.php
@@ -1,6 +1,6 @@
 <div class="pages__brands-form">
         <h2>Be part of avenue</h2>
         <div class="container row">
-                {{ gravity_form( 1, false, false ) }}
+                {{ gravity_form( 2, false, false ) }}
         </div>
     </div>
--- resources/views/partials/content-form-contact.blade.php
+++ resources/views/partials/content-form-contact.blade.php
@@ -1,6 +1,6 @@
 <div class="pages__contact-form">
                 <h2>Contact the avenue</h2>
         <div class="container row">
-                {{ gravity_form( 1, false, false ) }}
+                {{ gravity_form( 3, false, false ) }}
         </div>
     </div>
