57c802a749894bc4372bc0d82218175ee4380b23
Ken Tsang <ken@mediachain.co>
2019-07-02 10:30:18 +0100

    updates

--- resources/views/partials/content-team-profile.blade.php
+++ resources/views/partials/content-team-profile.blade.php
@@ -1,14 +1,22 @@
 <div class="pages__team-profile">
     <div class="row">
         <div class="pages__team-photo"><img src="@asset('images/team/img-placeholder.jpg')"/></div>
-        <div class="pages__team-content">content</div>
+        <div class="pages__team-content">
+            <h2>Name of person<span>Their Job Position</span></h2>
+            <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        </div>
     </div>
     <div class="row reverse">
         <div class="pages__team-photo"><img src="@asset('images/team/img-placeholder.jpg')"/></div>
-        <div class="pages__team-content">content</div>
+        <div class="pages__team-content">
+        <h2>Name of person<span>Their Job Position</span></h2>
+        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        </div>
     </div>
     <div class="row">
         <div class="pages__team-photo"><img src="@asset('images/team/img-placeholder.jpg')"/></div>
-        <div class="pages__team-content">content</div>
+        <div class="pages__team-content">
+        <h2>Name of person<span>Their Job Position</span></h2>
+        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>    
     </div>
 </div>
