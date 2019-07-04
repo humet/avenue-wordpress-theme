d6674b1a239f7ae994bb7a2bd1bd0e24e78b3d2a
Ken Tsang <ken@mediachain.co>
2019-07-04 11:57:06 +0100

    mobile menu start

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -5,5 +5,11 @@ export default {
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
 
+    $('.mobile-nav-button').click(function() {
+      $('#nav-icon2').toggleClass('open');
+      $('.mobile-nav-overlay').toggleClass('show');
+      $('body').toggleClass('no-scroll');
+  });
+
   },
 };
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,13 @@
 <header class="header">
-    <nav class="nav-primary">
-      <div class="container">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-      </div>
-    </nav>
+    <div class="mobile-nav-overlay">
+        <nav class="nav-primary">
+            <div class="container">
+                @if (has_nav_menu('primary_navigation'))
+                  {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+                @endif
+                </div>
+        </nav>
+    </div>
     <div class="header-bottom">
       <div class="container relative">
       <a class="brand" href="{{ home_url('/') }}"><img class="desktop-show" src="@asset('images/svg/logo.svg')"/><img class="mobile-show" src="@asset('images/svg/logo-white.svg')"/></a>
