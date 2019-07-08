f47648893695586dca7233f2cf86574c5f7d1f76
Ken Tsang <ken@mediachain.co>
2019-07-08 14:35:57 +0100

    updates

--- resources/views/partials/content-form-contact.blade.php
+++ resources/views/partials/content-form-contact.blade.php
@@ -1,5 +1,5 @@
 <div class="pages__contact-form">
-                <h2>Contact the avenue</h2>
+                <h2>Contact the Avenue</h2>
         <div class="container row">
                 {{ gravity_form( get_field('form_id_contact', 'options'), false, false ) }}
         </div>
--- resources/views/partials/content-homepage-join-avenue-form.blade.php
+++ resources/views/partials/content-homepage-join-avenue-form.blade.php
@@ -1,5 +1,5 @@
 <div class="pages__homepage-form">
-        <h2>Join the avenue</h2>
+        <h2>Join the Avenue</h2>
         <div class="container row">
                  {{ gravity_form( get_field('form_id_home', 'options'), false, false ) }}
         </div>
