ae9fb17fd01eb160ce7bd161f6e34739cc4331a4
Ken Tsang <ken@mediachain.co>
2019-07-10 12:23:26 +0100

    more updates on team pages

--- resources/views/partials/content-team-profile.blade.php
+++ resources/views/partials/content-team-profile.blade.php
@@ -1,6 +1,6 @@
 <div class="pages__team-profile">
     <div class="row mobile-column">
-        <div class="pages__team-photo"><img src="@asset('images/team/img-placeholder.jpg')"/></div>
+        <div class="pages__team-photo" style="background-image: url(@asset('images/team/img-placeholder.jpg'))";></div>
         <div class="pages__team-content">
             <h2>Name of person<span>Their Job Position</span></h2>
             <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
@@ -14,7 +14,7 @@
         </div>
     </div>
     <div class="row reverse mobile-column">
-        <div class="pages__team-photo"><img src="@asset('images/team/img-placeholder.jpg')"/></div>
+        <div class="pages__team-photo" style="background-image: url(@asset('images/team/img-placeholder.jpg'))";></div>
         <div class="pages__team-content">
         <h2>Name of person<span>Their Job Position</span></h2>
         <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
@@ -28,7 +28,7 @@
         </div>
     </div>
     <div class="row mobile-column">
-        <div class="pages__team-photo"><img src="@asset('images/team/ACBee.jpg')"/></div>
+        <div class="pages__team-photo" style="background-image: url(@asset('images/team/ACBee.jpg'))";></div>
         <div class="pages__team-content">
         <h2>AC Bee,<span>Avenue Course Leader</span></h2>
         <div class="text">As a media graduate, AC joined Social Chain and the new world of social-first production instead of following the expected route to TV. She has managed major campaigns for ASOS and Revolution and is a driving forces behind Avenue, believing there is rarely one set path.</div> 
@@ -41,7 +41,7 @@
           </ul>   
     </div>
     <div class="row reverse mobile-column">
-        <div class="pages__team-photo"><img src="@asset('images/team/KieraLawlor.jpg')"/></div>
+        <div class="pages__team-photo" style="background-image: url(@asset('images/team/KieraLawlor.jpg'))";></div>
         <div class="pages__team-content">
         <h2>Kiera Lawlor,<span>Director of Happiness for Social Chain</span></h2>
         <div class="text">Kiera nearly signed to Sony Music before joining Social Chain. As the agency’s Director of Happiness, she has created a value and culture-driven workplace that’s focused on mental wellbeing—a blueprint replicated by global brands and covered extensively in the press.</div>
