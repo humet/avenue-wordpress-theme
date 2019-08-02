7d91cc2d4b239505e2d7c7042bd4a37ec556218c
Ken Tsang <ken@mediachain.co>
2019-08-02 11:13:02 +0100

    update new images

--- resources/views/partials/content-team-profile.blade.php
+++ resources/views/partials/content-team-profile.blade.php
@@ -1,8 +1,8 @@
 <div class="pages__team-profile">
     <div class="row mobile-column">
-        <div class="pages__team-photo" style="background-image: url(@asset('images/team/img-placeholder.jpg'))";></div>
+        <div class="pages__team-photo background-top" style="background-image: url(@asset('images/team/steve.jpg'));";></div>
         <div class="pages__team-content">
-            <h2>Name of person<span>Their Job Position</span></h2>
+            <h2>Steven Bartlett<span>CEO, Social Chain</span></h2>
             <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
             <h3>Follow me</h3>
             <ul class="pages__team-social-links">
@@ -14,9 +14,9 @@
         </div>
     </div>
     <div class="row reverse mobile-column">
-        <div class="pages__team-photo" style="background-image: url(@asset('images/team/img-placeholder.jpg'))";></div>
+        <div class="pages__team-photo background-top" style="background-image: url(@asset('images/team/dom.jpg'))";></div>
         <div class="pages__team-content">
-        <h2>Name of person<span>Their Job Position</span></h2>
+        <h2>Dominic McGreor<span>COO, Social Chain</span></h2>
         <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
         <h3>Follow me</h3>
         <ul class="pages__team-social-links">
